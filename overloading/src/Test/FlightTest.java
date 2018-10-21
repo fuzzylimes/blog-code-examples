913d8431ec6b02e201aaf396c68d5fea36ab9d73
zcochran <fuzzylimes@gmail.com>
2018-10-21 10:22:47 -0400

    Files for 10-22-2018 post

---
+++ overloading/src/Test/FlightTest.java
@@ -0,0 +1,66 @@
+import org.junit.Assert;
+import org.junit.Test;
+import overloading.Flight;
+import overloading.Passenger;
+
+public class FlightTest {
+
+    @Test
+    public void add1PassengerTest() {
+        Flight f1 = new Flight();
+        f1.add1Passenger();
+        Assert.assertEquals(1, f1.getPassengers());
+        Assert.assertEquals(0, f1.getTotalCarryOns());
+        Assert.assertEquals(0, f1.getTotalCheckedBags());
+    }
+
+    @Test
+    public void add1PassengerCheckedBagTest() {
+        Flight f1 = new Flight();
+        f1.add1Passenger(3);
+        Assert.assertEquals(1, f1.getPassengers());
+        Assert.assertEquals(0, f1.getTotalCarryOns());
+        Assert.assertEquals(3, f1.getTotalCheckedBags());
+    }
+
+    @Test
+    public void add1Passenger2PassengersTest() {
+        Flight f1 = new Flight();
+        f1.add1Passenger(2);
+        f1.add1Passenger();
+        Assert.assertEquals(2, f1.getPassengers());
+        Assert.assertEquals(300, f1.getMaxCarryOns());
+        Assert.assertEquals(0, f1.getTotalCarryOns());
+        Assert.assertEquals(2, f1.getTotalCheckedBags());
+    }
+
+    @Test
+    public void add1PassengerBagsTest() {
+        Flight f1 = new Flight();
+        f1.add1Passenger(3, 2);
+        Assert.assertEquals(1, f1.getPassengers());
+        Assert.assertEquals(2, f1.getTotalCarryOns());
+        Assert.assertEquals(3, f1.getTotalCheckedBags());
+    }
+
+    @Test
+    public void add1PassengerBagTest() {
+        Flight f1 = new Flight();
+        Passenger bob = new Passenger(2, 4);
+        f1.add1Passenger(bob);
+        Assert.assertEquals(1, f1.getPassengers());
+        Assert.assertEquals(0, f1.getTotalCarryOns());
+        Assert.assertEquals(4, f1.getTotalCheckedBags());
+    }
+
+    @Test
+    public void add1PassengerBothBagTest() {
+        Flight f1 = new Flight();
+        Passenger bob = new Passenger(2, 4);
+        f1.add1Passenger(bob, 2);
+        Assert.assertEquals(1, f1.getPassengers());
+        Assert.assertEquals(2, f1.getTotalCarryOns());
+        Assert.assertEquals(4, f1.getTotalCheckedBags());
+    }
+
+}
