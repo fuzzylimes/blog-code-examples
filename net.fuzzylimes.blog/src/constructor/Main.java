6288129e2235eb06a0c9a6fd34f9a66682377004
zcochran <fuzzylimes@gmail.com>
2018-10-21 06:13:36 -0400

    Files for 10-21-2018 post

---
+++ net.fuzzylimes.blog/src/constructor/Main.java
@@ -0,0 +1,13 @@
+package constructor;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Passenger bob = new Passenger();
+        Passenger jane = new Passenger(2);
+        Passenger jill = new Passenger(2, 4);
+//        Passenger bill = new Passenger(50.0d);
+
+    }
+}
