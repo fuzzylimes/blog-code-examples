c00c1c9d74d76518388fe2146ca0c7af1b10648a
zcochran <fuzzylimes@gmail.com>
2018-10-27 14:47:20 -0400

    Code for 10/28 blog post

---
+++ skipunittests/src/test/java/DummyTestIT.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DummyTestIT {
+
+    @Test
+    public void anotherNormalUnitTest() {
+        Assert.assertFalse(false);
+    }
+
+}
---
+++ skipunittests/src/test/java/UnitTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UnitTest {
+
+    @Test
+    public void aNormalUnitTest() {
+        Assert.assertTrue(true);
+    }
+
+}
